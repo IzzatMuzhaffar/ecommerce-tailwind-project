3810273b8519224b799723e8181f13dfbb6b8ae9
Izzat <izzatmuz@gmail.com>
2024-12-14 23:40:52 +0800

    added product card and animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { useEffect } from "react"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Products from "./components/Products"
+import AOS from "aos"
+import "aos/dist/aos.css"
 
 const App = () => {
+
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <Hero />
+      <Products />
     </div>
   )
 }
